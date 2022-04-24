0a6e43e875c7aecf11c48aaed679bb33d0135d76
Sasi <sdhar0346@gmail.com>
2022-04-24 21:06:49 +0530

    added in page loading 0.5 complete

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ft1.style.display = 'none';
+
+function toggleHide1(){
+    let btn1 = document.getElementById('btn1');
+    let scont = document.getElementById('scont');
+    let ft1 = document.getElementById('site1f');
+    let fc = document.getElementsByClassName('Fcontainer');
+    // let p1 = document.getElementById('psite1');
+    let p1 = document.getElementById('ftext1');
+    ft1.style.display = 'none';
+
+    // if(ft1.style.display !='none'){
+    //     scont.style.display ='none';
+    //     ft1.style.display = 'grid';
+    //     // p1.style.display ='grid';
+    // }
+    // else{
+    //     scont.style.display ='grid';
+    //     ft1.style.display = 'none';
+    //     // p1.style.display ='none';
+    //     // fc.style.display = 'none';
+    // }
+   if(btn1= true){
+        scont.style.display ='none';
+        ft1.style.display = 'block';
+   }
+   else{
+    scont.style.display ='block';
+    ft1.style.display = 'none';
+   }
+}
