8dccdcb07ab6a91de3b32d411eacaa16a630f0f9
Sasi <sdhar0346@gmail.com>
2022-05-04 21:45:26 +0530

    pumping data to ftext divs from external texts

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-
-ft1.style.display = 'none';
-
-function toggleHide1(){
+   function toggleHide1(){
     let btn1 = document.getElementById('btn1');
     let scont = document.getElementById('scont');
+   //  let fcont = document.getElementById('Fcontainer');
     let ft1 = document.getElementById('site1f');
-    let fc = document.getElementsByClassName('Fcontainer');
+    let ft2 = document.getElementById('site2f');
+   //  let fc = document.getElementsByClassName('Fcontainer');
     // let p1 = document.getElementById('psite1');
-    let p1 = document.getElementById('ftext1');
     ft1.style.display = 'none';
-
-    // if(ft1.style.display !='none'){
-    //     scont.style.display ='none';
-    //     ft1.style.display = 'grid';
-    //     // p1.style.display ='grid';
-    // }
-    // else{
-    //     scont.style.display ='grid';
-    //     ft1.style.display = 'none';
-    //     // p1.style.display ='none';
-    //     // fc.style.display = 'none';
-    // }
+   //  ft2.style.display = 'none';
    if(btn1= true){
         scont.style.display ='none';
-        ft1.style.display = 'block';
+        ft1.style.display = 'block';   
    }
    else{
     scont.style.display ='block';
+   //  fcont.style.display ='none';
     ft1.style.display = 'none';
+    ft2.style.display = 'none';
    }
 }
+function load(){
+   let scont = document.getElementById('scont'); 
+   let C1 = document.getElementById('C1');
+   scont.style.display ='grid';
+   C1.style.display = 'none';
 
+}
 function back(){
     let scont = document.getElementById('scont');
     let ft1 = document.getElementById('site1f');
-
-
-    scont.style.display !='none';
-
-  
+    let C1 = document.getElementById('C1'); //new
+   //  let ft2 = document.getElementById('site2f');
+   scont.style.display ='grid';
+   // C1.style.display = 'none';
    ft1.style.display = 'none';
-//    ft2.style.display = 'none';
+   //  ft2.style.display = 'none';
    }
---
+++ script1.js
@@ -0,0 +1,364 @@
+  //  var btn1 = document.getElementById("btn1");
+  // var scont = document.getElementById("scont");
+  // var ft1 = document.getElementById("site1f");
+  // var fc = document.getElementsByClassName("Fcontainer");
+  // var ft2 = document.getElementById("site2f");
+  // var ft3 = document.getElementById("site3f");
+  // var ft13 = document.getElementById("site13f");
+function toggleHide1() {
+  let btn1 = document.getElementById("btn1");
+  let scont = document.getElementById("scont");
+  let ft1 = document.getElementById("site1f");
+  let fc = document.getElementsByClassName("Fcontainer");
+  // let p1 = document.getElementById('psite1');
+  let p1 = document.getElementById("ftext1");
+  ft1.style.display = "none";
+  if (btn1 = true) {
+    scont.style.display = "none";
+    ft1.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft1.style.display = "none";
+  }
+}
+
+function toggleHide2() {
+  let btn2 = document.getElementById("btn2");
+  let scont = document.getElementById("scont");
+  let ft2 = document.getElementById("site2f");
+  let fc = document.getElementsByClassName("Fcontainer");
+  // let p1 = document.getElementById('psite1');
+  // let p1 = document.getElementById("ftext1");
+  // ft1.style.display = "none";
+  if (btn2 =true) {
+    scont.style.display = "none";
+    ft2.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft2.style.display = "none";
+  }
+}
+function toggleHide3() {
+  let btn3 = document.getElementById("btn3");
+  let scont = document.getElementById("scont");
+  let ft3 = document.getElementById("site3f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn3 =true) {
+    scont.style.display = "none";
+    ft3.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft3.style.display = "none";
+  }
+}
+function toggleHide4() {
+  let btn4 = document.getElementById("btn4");
+  let scont = document.getElementById("scont");
+  let ft4 = document.getElementById("site4f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn4 =true) {
+    scont.style.display = "none";
+    ft4.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft4.style.display = "none";
+  }
+}
+function toggleHide5() {
+  let btn5 = document.getElementById("btn5");
+  let scont = document.getElementById("scont");
+  let ft5 = document.getElementById("site5f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn5 =true) {
+    scont.style.display = "none";
+    ft5.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft5.style.display = "none";
+  }
+}
+function toggleHide6() {
+  let btn6 = document.getElementById("btn6");
+  let scont = document.getElementById("scont");
+  let ft6 = document.getElementById("site6f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn6 =true) {
+    scont.style.display = "none";
+    ft6.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft6.style.display = "none";
+  }
+}
+function toggleHide7() {
+  let btn7 = document.getElementById("btn7");
+  let scont = document.getElementById("scont");
+  let ft7 = document.getElementById("site7f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn7 =true) {
+    scont.style.display = "none";
+    ft7.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft7.style.display = "none";
+  }
+}
+function toggleHide8() {
+  let btn8 = document.getElementById("btn8");
+  let scont = document.getElementById("scont");
+  let ft8 = document.getElementById("site8f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn8 =true) {
+    scont.style.display = "none";
+    ft8.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft8.style.display = "none";
+  }
+}
+function toggleHide8() {
+  let btn8 = document.getElementById("btn8");
+  let scont = document.getElementById("scont");
+  let ft8 = document.getElementById("site8f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn8 =true) {
+    scont.style.display = "none";
+    ft8.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft8.style.display = "none";
+  }
+}
+function toggleHide8() {
+  let btn8 = document.getElementById("btn8");
+  let scont = document.getElementById("scont");
+  let ft8 = document.getElementById("site8f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn8 =true) {
+    scont.style.display = "none";
+    ft8.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft8.style.display = "none";
+  }
+}
+function toggleHide9() {
+  let btn9 = document.getElementById("btn9");
+  let scont = document.getElementById("scont");
+  let ft9 = document.getElementById("site9f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn9 =true) {
+    scont.style.display = "none";
+    ft9.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft9.style.display = "none";
+  }
+}
+function toggleHide10() {
+  let btn10 = document.getElementById("btn10");
+  let scont = document.getElementById("scont");
+  let ft10 = document.getElementById("site10f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn10 =true) {
+    scont.style.display = "none";
+    ft10.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft10.style.display = "none";
+  }
+}
+function toggleHide11() {
+  let btn11 = document.getElementById("btn11");
+  let scont = document.getElementById("scont");
+  let ft11 = document.getElementById("site11f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn11 =true) {
+    scont.style.display = "none";
+    ft10.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft11.style.display = "none";
+  }
+}
+function toggleHide12() {
+  let btn12 = document.getElementById("btn12");
+  let scont = document.getElementById("scont");
+  let ft12 = document.getElementById("site12f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn12 =true) {
+    scont.style.display = "none";
+    ft12.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft12.style.display = "none";
+  }
+}
+function toggleHide13() {
+  let btn13 = document.getElementById("btn13");
+  let scont = document.getElementById("scont");
+  let ft13 = document.getElementById("site13f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn13 =true) {
+    scont.style.display = "none";
+    ft13.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft13.style.display = "none";
+  }
+}
+function toggleHide14() {
+  let btn14 = document.getElementById("btn14");
+  let scont = document.getElementById("scont");
+  let ft14 = document.getElementById("site14f");
+  let fc = document.getElementsByClassName("Fcontainer");
+   if (btn14 =true) {
+    scont.style.display = "none";
+    ft14.style.display = "block";
+  } 
+  else 
+  {
+    scont.style.display = "block";
+    ft14.style.display = "none";
+  }
+}
+
+function ffa1(){
+  $('#btn1').click(function() {
+  $('#ftext1').load('./texts/os-1.txt');
+})
+  }
+function ffa2(){
+  $('#btn2').click(function() {
+  $('#ftext2').load('./texts/os-2.txt');
+})
+  }
+function ffa3(){
+  $('#btn3').click(function() {
+  $('#ftext3').load('./texts/os-3.txt');
+})
+  }
+function ffa4(){
+  $('#btn4').click(function() {
+  $('#ftext4').load('./texts/os-4.txt');
+})
+  }
+function ffa5(){
+  $('#btn5').click(function() {
+  $('#ftext5').load('./texts/os-5.txt');
+})
+  }
+function ffa6(){
+  $('#btn6').click(function() {
+  $('#ftext6').load('./texts/os-6.txt');
+})
+  }
+function ffa7(){
+  $('#btn7').click(function() {
+  $('#ftext7').load('./texts/os-7.txt');
+})
+  }
+function ffa8(){
+  $('#btn8').click(function() {
+  $('#ftext8').load('./texts/os-8.txt');
+})
+  }
+function ffa9(){
+  $('#btn9').click(function() {
+  $('#ftext9').load('./texts/os-9.txt');
+})
+  }
+function ffa10(){
+  $('#btn10').click(function() {
+  $('#ftext10').load('./texts/os-10.txt');
+})
+  }
+function ffa11(){
+  $('#btn11').click(function() {
+  $('#ftext11').load('./texts/os-11.txt');
+})
+  }
+
+  function ffa12(){
+  $('#btn12').click(function() {
+  $('#ftext12').load('./texts/os-12.txt');
+})
+  }
+
+function ffa13(){
+  $('#btn13').click(function() {
+  $('#ftext13').load('./texts/os-13.txt');
+})
+  }
+
+function ffa14(){
+  $('#btn14').click(function() {
+  $('#ftext14').load('./texts/os-14.txt');
+})
+  }
+
+
+
+function back() {
+  let scont = document.getElementById("scont");
+  let ft1 = document.getElementById("site1f");
+  let ft2 = document.getElementById("site2f");
+  let ft3 = document.getElementById("site3f");
+  let ft4 = document.getElementById("site4f");
+  let ft5 = document.getElementById("site5f");
+  let ft6 = document.getElementById("site6f");
+  let ft7 = document.getElementById("site7f");
+  let ft8 = document.getElementById("site8f");
+  let ft9 = document.getElementById("site9f");
+  let ft10 = document.getElementById("site10f");
+  let ft11 = document.getElementById("site11f");
+  let ft12 = document.getElementById("site12f");
+  let ft13 = document.getElementById("site13f");
+  let ft14 = document.getElementById("site14f");
+
+ scont.style.display = "grid";
+ ft1.style.display = "none";
+ ft2.style.display = "none"; 
+ ft3.style.display = "none"; 
+ ft4.style.display = "none"; 
+ ft5.style.display = "none"; 
+ ft6.style.display = "none"; 
+ ft7.style.display = "none"; 
+ ft8.style.display = "none"; 
+ ft9.style.display = "none"; 
+ ft10.style.display = "none"; 
+ ft11.style.display = "none"; 
+ ft12.style.display = "none"; 
+ ft13.style.display = "none"; 
+ ft14.style.display = "none"; 
+}
