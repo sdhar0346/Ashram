6f051c97ff5c073a68061f3937c6533842a72b17
Sasi <sdhar0346@gmail.com>
2022-10-11 16:53:11 +0530

    edited more width of bios button width for mobile, buttoni margin deleted, Gallery-grouping,  and deities-images are pending

--- gitexe.bat
+++ gitexe.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "edited more width of bios button width for mobile, Gallery-grouping,  and deities-images are pending "
+git commit -m "edited more width of bios button width for mobile, buttoni margin deleted, Gallery-grouping,  and deities-images are pending "
 git push --all
